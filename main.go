695de17872434b06b9a566f371973399cd3e211c
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-05 22:19:57 +0800

    main.go: init telegram bot

--- main.go
+++ main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/go-telegram/bot"
+	"github.com/go-telegram/bot/models"
+)
+
 func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	b, err := bot.New(os.Getenv("BOT_TELEGRAM_TOKEN"), bot.WithDefaultHandler(handler))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b.Start(ctx)
+}
+
+func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
 }
