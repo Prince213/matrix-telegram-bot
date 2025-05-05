4dc26de563b875f43feccca03f8e65087ee2ce0b
Sizhe Zhao <prc.zhao@outlook.com>
2025-05-05 22:23:40 +0800

    main.go: fetch image

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"log"
+	"net/http"
 	"os"
 	"os/signal"
 
@@ -23,4 +24,25 @@ func main() {
 }
 
 func handler(ctx context.Context, b *bot.Bot, update *models.Update) {
+	m := update.Message
+	if m == nil || len(m.Photo) == 0 {
+		return
+	}
+
+	// TODO: which one to download
+	photo := m.Photo[len(m.Photo)-1]
+
+	file, err := b.GetFile(ctx, &bot.GetFileParams{FileID: photo.FileID})
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	resp, err := http.Get(b.FileDownloadLink(file))
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
+	_ = resp
 }
